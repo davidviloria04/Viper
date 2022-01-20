6347b352acf2a7d4af65144389afee61c98fdc2d
ValerieNamuche <55290243+ValerieNamuche@users.noreply.github.com>
2022-01-20 18:54:48 -0500

    size for cell

--- Viper- 02/TableView/Countries/Mod1TitleTableViewCell.swift
+++ Viper- 02/TableView/Countries/Mod1TitleTableViewCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AssistantKit
 
 class Mod1TitleTableViewCell: UITableViewCell{
     
@@ -18,22 +19,69 @@ class Mod1TitleTableViewCell: UITableViewCell{
     private var indexPath: IndexPath?
     var delegate : TVViewController?
     var listCities = [Cities]()
+    let family = Device.screen.family
+    var sizeh = 0
+    var sizew = 0
     
     override func awakeFromNib() {
         super.awakeFromNib()
         citiesCV.dataSource = self
         citiesCV.delegate = self
         register(classType: CollectionViewCell.self)
+        screenSizeH()
+        screenSizeW()
         self.citiesCV.collectionViewLayout = flowLayout
+        
         citiesCV.reloadData()
 
         // Initialization code
+    }
+    var flowLayout: UICollectionViewFlowLayout {
+        let flowLayout = UICollectionViewFlowLayout()
+        // edit properties here
+        
+        flowLayout.itemSize = CGSize(width: sizew , height: sizeh)
+        flowLayout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        flowLayout.minimumInteritemSpacing = 4
+        // edit properties here
+
+        return flowLayout
     }
     
     private func register(classType: AnyClass) {
             let id = String(describing: classType.self)
         citiesCV.register(UINib(nibName: id, bundle: nil), forCellWithReuseIdentifier: id)
         }
+    
+    public func screenSizeH(){
+        if (family == .old) {
+            return sizeh = 50
+        }
+        if (family == .medium) {
+            return sizeh = 80
+        }
+        if (family == .small) {
+            return sizeh = 70
+        }
+        if (family == .big) {
+            return sizeh = 100
+        }
+    }
+    
+    public func screenSizeW(){
+        if (family == .old) {
+            return sizew = 60
+        }
+        if (family == .medium) {
+            return sizew = 80
+        }
+        if (family == .small) {
+            return sizew = 70
+        }
+        if (family == .big) {
+            return sizew = 100
+        }
+    }
     
 }
 
@@ -49,18 +97,10 @@ extension Mod1TitleTableViewCell : UICollectionViewDelegate, UICollectionViewDat
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)  {
         delegate?.alert()
+        debugPrint(family)
     }
     
 }
-var flowLayout: UICollectionViewFlowLayout {
-    let flowLayout = UICollectionViewFlowLayout()
-    // edit properties here
-    flowLayout.itemSize = CGSize(width: 80, height: 80)
-    flowLayout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
-    flowLayout.minimumInteritemSpacing = 4
-    // edit properties here
 
-    return flowLayout
-}
 
 

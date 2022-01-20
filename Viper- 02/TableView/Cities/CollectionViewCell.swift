6f8f0e5d306cd5d6b8e996431c0d54c6b5b21da8
David Viloria <david.viloria0424@gmail.com>
2022-01-20 16:27:49 -0500

    Se cambia color de fondo

--- Viper- 02/TableView/Cities/CollectionViewCell.swift
+++ Viper- 02/TableView/Cities/CollectionViewCell.swift
@@ -13,6 +13,7 @@ class CollectionViewCell: UICollectionViewCell {
     //View #1
     @IBOutlet weak var cityname: UILabel!
     @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var view: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
